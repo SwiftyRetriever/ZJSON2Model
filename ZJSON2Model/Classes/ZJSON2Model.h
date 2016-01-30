093fcdc1f8ad4e2b520fe2cfa26bc4a3591f4aa5
GSTF <GSTF@gstfdemac-mini.local>
2016-01-30 17:02:05 +0800

    switch function to NSObject categary  and add Array categary.

---
+++ ZJSON2Model/Classes/ZJSON2Model.h
@@ -0,0 +1,10 @@
+//
+//  ZJSON2Model.h
+//  ZJSON2Model
+//
+//  Created by ZERO. on 16/1/30.
+//  Copyright © 2016年 ZERO. All rights reserved.
+//
+
+#import "NSObject+ZJSON2Model.h"
+#import "NSArray+ZJSON2Model.h"
